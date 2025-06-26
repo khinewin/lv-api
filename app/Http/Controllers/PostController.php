6567227777fb9c15a4607833919bc24ca866d1c5
khinewin <khinewin123@gmail.com>
2025-06-26 09:54:16 +0630

    api

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function updatePost(Request $request){
+      //  return response()->json($request->all());
+          $request->validate([
+            'title'=>"required", 
+            "content"=>"required"
+        ]);
+        $id=$request->id;
+        $post=Post::whereId($id)->first();
+        if($post){
+            $post->title=$request->title;
+            $post->content=$request->content;
+            $post->update();
+            $data=["status"=>"success", "data"=>$post];
+            return response()->json($data, 200);
+        }else{
+             $data=["status"=>"failed", "msg"=>"Post not found."];
+            return response()->json($data, 404);
+        }
+    }
+    public function getPostById($id){
+        $post=Post::whereId($id)->first();
+        if($post){
+            $data=["status"=>"success", "data"=>$post];
+            return response()->json($data, 200);
+        }else{
+            $data=["status"=>"failed", "msg"=>"Post not found."];
+            return response()->json($data, 404);
+        }
+    }
+    public function createPost(Request $request){
+        $request->validate([
+            'title'=>"required", 
+            "content"=>"required"
+        ]);
+        $post=new Post();
+        $post->title=$request->title;
+        $post->content=$request->content;
+        $post->save();
+        $data=["status"=>"success", "data"=>$post];
+        return response()->json($data, 201);
+    }
+    public function getPosts(){
+        $posts=Post::OrderBy("id", "desc")->get();
+        return response()->json($posts, 200);
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>My Backend API Doc</h1>
+    <h4>Get All Posts</h4>
+    <ul>
+        <li>Method : GET</li>
+        <li>URL : "/api/posts"</li>
+    </ul>
+    <h4>Get Post By Id</h4>
+    <ul>
+        <li>Method : GET</li>
+        <li>URL : "/api/posts/id/{id}"</li>
+    </ul>
+
+    <h4>Create Post</h4>
+    <ul>
+        <li>Method : POST</li>
+        <li>URL : "/api/posts"</li>
+        <li>Property : {"title", "content"}</li>
+    </ul>
+     <h4>Update Post</h4>
+    <ul>
+        <li>Method : PUT</li>
+        <li>URL : "/api/posts"</li>
+        <li>Property : {"id", "title", "content"}</li>
+    </ul>
+</body>
+</html>
